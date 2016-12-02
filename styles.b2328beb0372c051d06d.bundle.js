464da12bf1ce8c99d51b25b45556188acc2b35b9
Hugo Saracino <hugo.saracino@gmail.com>
2016-12-02 10:56:04 +0100

    Added About Page and Tooltips

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{417:function(n,o){},973:function(n,o,c){n.exports=c(417)}},[973]);
+webpackJsonp([1,2],{420:function(n,o){},978:function(n,o,c){n.exports=c(420)}},[978]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
